05c18dc77fd19b3a4f1f87bcd0a34c0aa5fe1e33
root <obimpealf99@gmail.com>
2023-02-23 15:46:08 -0800

    task 2

---
+++ 0x03-debugging/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+int main(void)
+{
+int a = 10;
+int b = 20;
+int c = 30;
+int largest = largest_number(a, b, c);
+printf("The largest number is: %d\n", largest);
+return (0);
+}
