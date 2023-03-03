a9cdae9c96f41b87144a0b6de23667c23abd55f5
root <obimpealf99@gmail.com>
2023-03-03 04:58:16 -0800

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+/**
+ * _strncat - concatenates two strings, using at most n bytes from src
+ * @dest: pointer to destination string
+ * @src: pointer to source string
+ * @n: maximum number of bytes to use from src
+ *
+ * Return: pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0;
+	int i;
+
+	/* Find the length of dest string */
+	while (dest[dest_len] != '\0')
+			{
+				dest_len++;
+			}
+
+	/* Append up to n bytes from src to dest */
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+
+	/* Add null terminator to the end of the resulting string */
+	dest[dest_len + i] = '\0';
+
+	/* Return the resulting string */
+	return dest;
+}
